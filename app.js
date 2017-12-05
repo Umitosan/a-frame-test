e1378863b55691ada84cd498f25e51cd14ec36d5
Dominic Brown <yeahdom@gmail.com>
2017-12-04 21:58:57 -0800

    add orbit test objects, add looped animation

--- app.js
+++ app.js
@@ -3,12 +3,17 @@
 function makeBoxes(total) {
   for (var i=0; i<(2*total) ;i+=2) {
     var xPos = i-total;
-    var myEnt = $('<a-entity>').attr({  class: 'BOXY',
+    var myDelay = i*100;
+    var myEnt = $('<a-entity>').attr({  class: 'BOXY' + i,
                                         geometry: 'primitive: box; width: 1; height: 1',
                                         position:  xPos + ' 7 -5',
                                         material: 'color: #33DDEE; shader: flat'
                                         });
+    var animSpin = $('<a-animation>').attr({ mixin: 'animSpin1',
+                                             delay: myDelay
+                                             });
     $('#scene1').append(myEnt);
+    $('.BOXY' + i).append(animSpin);
   }
   console.log("makeBoxes finished");
 } // makeBoxes
